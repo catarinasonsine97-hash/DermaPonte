fecb7738da397cef0bfa254fd754af8228ee03b3
Catarina Sonsine <catarinasonsine97@gmail.com>
2026-09-17 16:06:31 -0300

    feat(java): add initial backend DermaPonteApplication.java

---
+++ backend-java/src/main/java/br/com/dermaponte/DermaPonteApplication.java
@@ -0,0 +1,11 @@
+package br.com.dermaponte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermaPonteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DermaPonteApplication.class, args);
+    }
+}
